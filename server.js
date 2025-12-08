312da03732ae5e62f3af1e9d085cabec3f729433
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2025-12-08 12:29:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -130,5 +130,5 @@ app.get("/api/quiz", (req, res) => {
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
+  console.log(`✅ Server running on http://192.168.30.23:${PORT}`);
 });
